0bad928d58a402475169b81b6a6f893cca6f7c2a
Fan <garyvan4@qq.com>
2023-07-01 10:27:03 +0800

    创建

---
+++ src/main/java/org/fjl/egoing/EgoingApplication.java
@@ -0,0 +1,13 @@
+package org.fjl.egoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgoingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fjl/egoing/EgoingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fjl.egoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
